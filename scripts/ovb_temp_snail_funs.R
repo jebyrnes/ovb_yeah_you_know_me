613f3e4f10c4971f4f0f0f3694111969f86752f4
jebyrnes <jarrett.byrnes@umb.edu>
2022-03-08 16:22:35 -0500

    Added site RE to simulations

--- scripts/ovb_temp_snail_funs.R
+++ scripts/ovb_temp_snail_funs.R
@@ -89,13 +89,13 @@ analyze_plots <- function(plot_df){
     ~model_type, ~fit,
     "Naive", lm(snails ~ plot_temp, data = plot_df),
     "RE", lmer(snails ~ plot_temp + (1|site), data = plot_df),
-    "First Differences", lm(delta_snails ~ delta_temp,data = plot_df), #fix SE?
     "FE with Dummy Variables", lm(snails ~ plot_temp + site, data = plot_df),
     "FE Using Mean Differencing", lm(snails ~ plot_temp + site, data = plot_df), #fix SE?
     "Group Mean Covariate", lmer(snails ~ plot_temp + site_mean_temp + (1|site), data = plot_df),
     "Group Mean Centered", lmer(snails ~ plot_temp_dev + site_mean_temp + (1|site), data = plot_df),
     "Group Mean Covariate, no RE", lm(snails ~ plot_temp + site_mean_temp, data = plot_df),
-    "Group Mean Centered, no RE", lm(snails ~ plot_temp_dev + site_mean_temp, data = plot_df)
+    "Group Mean Centered, no RE", lm(snails ~ plot_temp_dev + site_mean_temp, data = plot_df),
+    "First Differences", lm(delta_snails ~ delta_temp,data = plot_df) #fix SE?
     
   ) %>%
     mutate(coefs = map(fit, tidy), #get coefficients with broom
