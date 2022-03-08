30cf908261dc515bc82b21a6bdc3c64e80e77ea2
jebyrnes <jarrett.byrnes@umb.edu>
2022-03-08 11:31:32 -0500

    adding site RE to diagram

--- scripts/make_ovb_diagram.R
+++ scripts/make_ovb_diagram.R
@@ -40,28 +40,30 @@ make_snail_graph <- function(or = 1,
                              ts = 4,
                              rsd = 0,
                              tsd = 0,
+                             ssd = 1,
                              esd = 1,
                              lsd = 3){
   
   snail_simple <- create_graph(
     nodes_df = create_node_df(
-      n = 6,
+      n = 7,
       label = c(glue("temperature\nsd = {tsd}"), 
                 "snails", 
                 glue("recruitment\nsd = {rsd}"), 
                 "oceanography", 
                 glue("residual\nsd = {esd}"),
-                glue("local temp var\nsd = {lsd}")),
-      shape = c("rectangle", "rectangle", "ellipse", "ellipse", "circle", "rectangle"),
+                glue("local temp var\nsd = {lsd}"),
+                glue("site\nsd = {ssd}")),
+      shape = c("rectangle", "rectangle", "ellipse", "ellipse", "circle", "rectangle", "circle"),
       fixedsize   = FALSE,
-      x = c(2,4,2,0,4.7, 0),
-      y = c(1,2,3,2, 3.2, 1)),
+      x = c(2,4,2,0,5.5, 0, 4),
+      y = c(1,2,3,2, 3.2, 1, 3.2)),
     
     edges_df = create_edge_df(
-      from = c(1,3,4,4,5, 6),
-      to = c(2,2,1,3,2, 1),
+      from = c(1,3,4,4,5, 6, 7),
+      to = c(2,2,1,3,2, 1, 2),
       rel = "leading_to",
-      label = c(ts, rs, ot, or, 1, 1))
+      label = c(ts, rs, ot, or, 1, 1,1))
   ) %>% 
     set_graph_theme
   
