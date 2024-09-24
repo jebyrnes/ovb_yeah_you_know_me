d82c3e8ecc775674121ed594c75ade2d183926a8
jebyrnes <jarrett.byrnes@umb.edu>
2024-09-24 14:23:47 -0400

    Fix typo in filenames

--- supporting_information_s6_s7/pandoc_script.sh
+++ supporting_information_s6_s7/pandoc_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc -s appendix_one_models_and_sims.html -o appendix_one_models_and_sims.docx
-pandoc -s appendix_two_implementation.html -o appendix_two_implementation.docx
+pandoc -s supporting_information_s6.html -o supporting_information_s6.docx
+pandoc -s supporting_information_s7.html -o supporting_information_s7.docx
